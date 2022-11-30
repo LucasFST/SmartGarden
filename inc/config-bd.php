6ffcf45c72974c8c472f9d32e666a9ba46bdcf7e
LucasFST <115565231+LucasFST@users.noreply.github.com>
2022-12-01 00:18:51 +0100

    Update config-bd.php

--- inc/config-bd.php
+++ inc/config-bd.php
@@ -1,8 +1,8 @@
 <?php
 	define('SERVEUR', 'localhost');
-	define('UTILISATRICE', 'p2020351'); // votre login, par exemple p1234567
-	define('MOTDEPASSE', 'Dollar20Twerp'); // votre mot de passe, par exemple abcd1234
-	define('BDD', 'p2020351'); // votre BD, par exemple p1234567
+	define('UTILISATRICE', 'monLogin'); // votre login, par exemple p1234567
+	define('MOTDEPASSE', 'monMotDePasse'); // votre mot de passe, par exemple abcd1234
+	define('BDD', 'maBDD'); // votre BD, par exemple p1234567
 ?>
 
 
